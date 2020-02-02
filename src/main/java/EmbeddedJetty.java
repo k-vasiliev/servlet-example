824b78017c68e691ca72ac7edcbd62ff4f99e5c4
Kirill Vasilev <ki.vasilyev@hh.ru>
2020-02-02 22:38:08 +0300

    Jetty with jersey

--- src/main/java/EmbeddedJetty.java
+++ src/main/java/EmbeddedJetty.java
@@ -1,13 +1,24 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 public class EmbeddedJetty {
 
   private static Server createServer(int port) {
     Server server = new Server(port);
+
+    // config resources, jersey servlet
+    ResourceConfig resourceConfig = new ResourceConfig() {{
+      packages("resource");
+    }};
+    ServletContainer container = new ServletContainer(resourceConfig);
+    ServletHolder holder = new ServletHolder(container);
+
     ServletHandler servletHandler = new ServletHandler();
-    servletHandler.addServletWithMapping(ExampleServlet.class, "/");
+    servletHandler.addServletWithMapping(holder, "/");
     server.setHandler(servletHandler);
     return server;
   }
---
+++ src/main/java/resource/ExampleResource.java
@@ -0,0 +1,15 @@
+package resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class ExampleResource {
+
+  @GET
+  @Path(value = "/")
+  public Response get() {
+    return Response.ok("Hello!").build();
+  }
+}
